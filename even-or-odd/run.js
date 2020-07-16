23749d98619daf921e60073e21ff9353a993c229
Matthew Nitschke <matthewcnitschke@gmail.com>
2020-07-15 21:19:25 -0600

    added

--- even-or-odd/run.js
+++ even-or-odd/run.js
@@ -1,10 +1,57 @@
 const robot = require("robotjs");
+const inquirer = require('inquirer');
 
+function sumHex(hex) {
+    var bigint = parseInt(hex, 16);
+    var r = (bigint >> 16) & 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
 
-// Get mouse position.
-const mouse = robot.getMousePos();
+    return r + g + b;
+}
 
-// Get pixel color in hex format.
-const hex = robot.getPixelColor(mouse.x, mouse.y);
+let topLeft;
+let bottomRight;
 
-console.log("#" + hex + " at x:" + mouse.x + " y:" + mouse.y);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'topLeft',
+            validate: () => {
+                topLeft = robot.getMousePos();
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'bottomRight',
+            validate: () => {
+                bottomRight = robot.getMousePos();
+                return true;
+            }
+        }
+    ])
+    .then(() => {
+        let w = bottomRight.x - topLeft.x;
+        let h = bottomRight.y - topLeft.y;
+
+        let img = robot.screen.capture(
+            topLeft.x, 
+            topLeft.y, 
+            w, 
+            h
+        );
+        
+        let sum = 0;
+        for (let x = 0; x < w; x ++) {
+            for (let y = 0; y < h; y++) {
+                let pixelHex = img.colorAt(x, y);
+                let pixelSum = sumHex(pixelHex);
+
+                sum += pixelSum;
+            }
+        }
+
+        console.log(sum)
+    })
